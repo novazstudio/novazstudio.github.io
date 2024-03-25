e1eb451f9fd007c954e91d8d658984cb4e81682f
novazstudio <157850268+novazstudio@users.noreply.github.com>
2024-03-24 21:28:14 -0500

    Solución de errores
    Se soluciono el error del botón de descargar la aplicación

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
 document.getElementById('enlace-descarga').addEventListener('click', function() {
   // Cambia 'URL_DEL_ARCHIVO' por la URL real del archivo que deseas descargar
-  var urlArchivo = 'https://www.mediafire.com/file/4c74stt1uzr32vp/NovBooks.apk/file';
-  var nombreArchivo = 'NovBooks'; // Puedes cambiar 'nombre_del_archivo' por el nombre que quieras para el archivo
+  var urlArchivo = 'https://ia600206.us.archive.org/30/items/nov-books/NovBooks.apk';
   
-  // Crear una solicitud para obtener el archivo
-  var solicitud = new XMLHttpRequest();
-  solicitud.open('GET', urlArchivo, true);
-  solicitud.responseType = 'blob'; // El tipo de respuesta es un blob (archivo binario)
+  // Crear un enlace temporal para descargar el archivo
+  var enlaceDescarga = document.createElement('a');
+  enlaceDescarga.href = urlArchivo;
+  enlaceDescarga.setAttribute('download', ''); // Dejar el atributo 'download' vacío
   
-  // Cuando la solicitud esté completa
-  solicitud.onload = function() {
-    if (solicitud.status === 200) {
-      // Crear un enlace temporal para descargar el archivo
-      var enlaceDescarga = document.createElement('a');
-      enlaceDescarga.href = window.URL.createObjectURL(solicitud.response);
-      enlaceDescarga.setAttribute('download', nombreArchivo);
-      
-      // Simular un clic en el enlace temporal
-      enlaceDescarga.click();
-    }
-  };
-  
-  // Enviar la solicitud
-  solicitud.send();
+  // Simular un clic en el enlace temporal
+  enlaceDescarga.click();
 });
